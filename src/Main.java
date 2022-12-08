0335e80f2a3ed59019d6e80c499b77b323ff3ad1
Trofimov Dmitry <winprint63@gmail.com>
2022-12-08 20:42:46 +0400

    scanner added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Print calculator begin!");
         System.out.println("Select options");
+
+    Scanner firstNum = new Scanner(System.in);
+        System.out.println("Введите первый параметр");
+        int chooseNum1 = firstNum.nextInt();
+        System.out.println("вы выбрали " + chooseNum1);
     }
 }
