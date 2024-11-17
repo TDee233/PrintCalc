f24f9ef73a089c3198d1cbc64b6650805e589dcf
Trofimov Dmitry <winprint63@gmail.com>
2024-11-17 18:54:12 +0400

    classes added

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
 
         System.out.println("Print calculator begin!");
         System.out.println("Select options");
-
-    Scanner firstNum = new Scanner(System.in);
+        Scanner firstNum = new java.util.Scanner(System.in);
         System.out.println("Введите первый параметр");
         int chooseNum1 = firstNum.nextInt();
         System.out.println("вы выбрали " + chooseNum1);
+
+
     }
 }
