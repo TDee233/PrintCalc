c15afafc900b4015eea6948d3ba49a1bb863cedb
Trofimov Dmitry <winprint63@gmail.com>
2024-12-01 19:02:56 +0400

    Calculator class changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Calculator {
@@ -9,10 +10,20 @@ public class Calculator {
 
     public void calculateCost() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите количество единиц продукции: ");
+
+        System.out.println("Выберите категорию: \n 1 - визитки\n 2 - пластиковые карты");
+        int chooseCat = scanner.nextInt();
+        if (chooseCat == 1) {
+            System.out.println("Вы выбрали категорию Визитки");
+        }
+        else System.out.println("Вы выбрали категорию Пластиковые карты");
+
+        System.out.println("Введите количество единиц продукции: ");
         int quantity = scanner.nextInt();
 
-        System.out.println("Выберите изделие: " + scanner.nextInt());
+        System.out.println("Расчет для количества: " + quantity);
+
+
 
         double totalCost = quantity * materials.getPricePerUnit();
         System.out.printf("Общая стоимость: %.2f\n", totalCost);
