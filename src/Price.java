52c091b292d6eca3f60f2f463a681f9de08acee2
Trofimov Dmitry <winprint63@gmail.com>
2024-12-07 20:02:02 +0400

    Price changes

--- src/Price.java
+++ src/Price.java
@@ -7,3 +7,19 @@ final double tShirt = 850;
 final double banner = 550;
 
 }
+
+//public class MyClass {
+//    private final int myFinalVariable = 10;
+//
+//    public int getMyFinalVariable() {
+//        return myFinalVariable;
+//    }
+//}
+//
+
+//public class AnotherClass {
+//    public void displayValue() {
+//        MyClass myClass = new MyClass();
+//        System.out.println("Final variable value: " + myClass.getMyFinalVariable());
+//    }
+//}
