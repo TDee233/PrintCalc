cc4c74be5a9fa655c53fa9d846d1382df6b92d42
Trofimov Dmitry <winprint63@gmail.com>
2024-12-24 20:12:10 +0400

    Price fix

--- src/Price.java
+++ src/Price.java
@@ -3,6 +3,7 @@ public class Price {
 final double businessCard = 1;
 final double interiorPrint = 850;
 final double plasticCard = 18;
+final double goldPlasticCard = 25;
 final double tShirt = 850;
 final double banner = 550;
 final double sticker = 10;
